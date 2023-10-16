correct half precision test

test.py
@@ -2,6 +2,7 @@ from einops import rearrange
 import torch
 import torch.nn as nn
 from torch_discounted_cumsum import  discounted_cumsum_left, discounted_cumsum3_left
+import numpy as py
 
 class Discounted_Cumsum(nn.Module):
     """
@@ -55,7 +56,8 @@ class Discounted_Cumsum(nn.Module):
             tensor = tensor.view(-1, _shape[-1])  # (B*D*H, S)
         elif self.dim_head == 1 and self.dim_leng == 2:
             if len(_shape) == 4:
-                tensor = rearrange(tensor, 'B H S D -> (B D) H S')
+                tensor = tensor.permute(0,3,1,2).flatten(0,1)
+                #tensor = rearrange(tensor, 'B H S D -> (B D) H S')
             elif len(_shape) == 5:
                 tensor = rearrange(tensor, 'B H S D1 D2 -> (B D1 D2) H S')
             else:
@@ -67,7 +69,8 @@ class Discounted_Cumsum(nn.Module):
         tensor = discounted_cumsum3_left(tensor, gamma)
         if len(_shape) == 4:
             B, H, S, D = _shape
-            tensor = rearrange(tensor, '(B D) H S -> B H S D', B=B)
+            tensor = tensor.reshape(B,D,H,S).permute(0,2,3,1)
+            #tensor = rearrange(tensor, '(B D) H S -> B H S D', B=B)
         elif len(_shape) == 5:
             B, H, S, D1, D2 = _shape
             tensor = rearrange(
@@ -83,9 +86,9 @@ class ParallelRetention_fast(nn.Module):
         self.gamma_cusum_1 = Discounted_Cumsum(1,2)
         self.gamma_cusum_2 = Discounted_Cumsum(1,2)
     
-    def forward(self, q, k, v, omask):
-        gamma = omask[0,:,1,0].float()
-        L     = omask.sum(dim=-1).sqrt()[...,None]
+    def forward(self, q, k, v, omask=None, gamma=None, L=None):
+        if gamma is None:gamma = omask[0,:,1,0].float()
+        if L is None:L     = omask.sum(dim=-1,keepdim=True)
         qL    = q/L
         Tbf   = self.gamma_cusum_1(k,gamma)
         P     = torch.einsum('BHia, BHia->BHi',qL, Tbf)
@@ -97,7 +100,7 @@ class ParallelRetention_fast(nn.Module):
 
 class ParallelRetention_reduce(nn.Module):
     
-    def forward(self, q, k, v, omask):
+    def forward(self, q, k, v, omask,*args,**kargs):
         q_bar_coef = omask[...,:,0]/omask.sum(dim=-1).sqrt()
         k_bar_coef = 1/(omask[...,:,0])#<----this will overflow~~~~!!!!
         q_bar = q_bar_coef[...,None]*q
@@ -105,15 +108,16 @@ class ParallelRetention_reduce(nn.Module):
         T = torch.cumsum(k_bar,dim=-2)
         P = torch.einsum('BHia,BHia->BHi', T,q_bar)
         P = P[...,None].detach().abs().clamp(min=1)
+        q_bar = q_bar/P
         D = torch.einsum('BHia,BHic->BHiac',k_bar, v)
         D = torch.cumsum(D,dim=-3)
-        O = torch.einsum('BHia,BHiac->BHic',q_bar,D)/P
+        O = torch.einsum('BHia,BHiac->BHic',q_bar,D)
         return O
 
 class ParallelRetention_origin(nn.Module):
     
     
-    def forward(self, q, k, v, omask):
+    def forward(self, q, k, v, omask,*args,**kargs):
         mask = omask / omask.sum(dim=-1, keepdim=True).sqrt()
         mask = torch.nan_to_num(mask, nan=0.0)
         decay_mask = mask
@@ -149,42 +153,69 @@ if __name__ == "__main__":
     layer3= ParallelRetention_origin()
     records = []
     configs = []
-    H = 16
-    for B in [1, 10]:
-        for S in [10, 30, 100, 1000, 2000]:
-            for D1 in [8, 16, 32]:
+    
+    for B in [1]:
+        for S in [32, 64, 128, 256, 512, 1024]:
+            for D1 in [8, 16, 32, 64]:
                 #for D2 in [64]:
-                    configs.append([B,S,D1,D1])
-    for B,S,D1,D2 in tqdm(configs):
+                for H in [16]:
+                    configs.append([B,H,S,D1,D1])
+    records = []
+    for B,H,S,D1,D2 in tqdm(configs):
         
-        q     =  torch.randn(B,H,S,D1).cuda()#  float16 and bfloat16 always get wrong result ###.half()
-        k     =  torch.randn(B,H,S,D1).cuda()#  float16 and bfloat16 always get wrong result ###.half()
-        v     =  torch.randn(B,H,S,D2).cuda()#  float16 and bfloat16 always get wrong result ###.half()
-        omask =         get_omask(S,H).cuda()#  float16 and bfloat16 always get wrong result ###.half()
-        O1 = layer1(q,k,v,omask)
-        O2 = layer2(q,k,v,omask)
-        O3 = layer3(q,k,v,omask)
+        q     =  (torch.randn(B,H,S,D1).cuda()/np.sqrt(S*D1))#  float16 and bfloat16 may get wrong result if the q@k@v too large. Normlize it thus q@k@v be a normal distribution
+        k     =  (torch.randn(B,H,S,D1).cuda()/np.sqrt(S*D1))#  float16 and bfloat16 may get wrong result if the q@k@v too large. Normlize it thus q@k@v be a normal distribution
+        v     =  (torch.randn(B,H,S,D2).cuda()/np.sqrt(S*D2))#  float16 and bfloat16 may get wrong result if the q@k@v too large. Normlize it thus q@k@v be a normal distribution
+        omask =                         get_omask(S,H).cuda()#  float16 and bfloat16 always get wrong result ###.half()
+        gamma = omask[0,:,1,0].float()
+        L     = omask.sum(dim=-1).sqrt()[...,None]
+        O1 = layer1(q,k,v,omask, gamma, L)
+        O2 = layer2(q,k,v,omask, gamma, L)
+        O3 = layer3(q,k,v,omask, gamma, L)
         e1 = torch.dist(O1,O3).item()
         e2 = torch.dist(O2,O3).item()
-        record = [B,S,D1,D2,e1,e2]
+        record = [B,H,S,D1,D2,e1,e2]
         #print(record)
         
-        for model in [layer1, layer2, layer3]:
-            try:
-                costs = []
-                for _ in range(100):
-                    now = time.time()
-                    O = model(q,k,v,omask)
-                    #O.mean().backward()
-                    cost = time.time()-now
-                    costs.append(cost)
-                cost = np.mean(cost)
-            except:
-                cost = np.nan
+        for model in [layer1, 
+                      layer2, 
+                      layer3]:
+            costs = []
+            for _ in range(100):
+                now = time.time()
+                O = model(q,k,v,omask, gamma, L)
+                #O.mean().backward()
+                cost = time.time()-now
+                costs.append(cost)
+            cost = np.mean(cost)
+            
             record.append(cost)
         records.append(record)
-    dataframe = pd.DataFrame(records, columns=['B','S','D1','D2','e1','e2','fast','reduce','origin'])
+    dataframe = pd.DataFrame(records, columns=['B','H','S','D1','D2','e1','e2','fast','reduce','origin'])
     dataframe['speed_up_fast'] = np.round(dataframe['origin']/dataframe['fast'],3)
     dataframe['speed_up_reduce'] = np.round(dataframe['origin']/dataframe['reduce'],3)
     print(dataframe)
     dataframe.to_csv('benchmark.csv',index=False)
+
+    ####### percision case
+    # for B in [1]:
+    #     for S in [10, 50, 100,150, 200, 500, 1000, 2000]:
+    #         for D1 in [ 128]:
+    #             #for D2 in [64]:
+    #             for H in [16]:
+    #                 configs.append([B,H,S,D1,D1])
+    # records = []
+    # for B,H,S,D1,D2 in tqdm(configs):
+        
+    #     q     =  torch.randn(B,H,S,D1).cuda()#  float16 and bfloat16 always get wrong result ###.half()
+    #     k     =  torch.randn(B,H,S,D1).cuda()#  float16 and bfloat16 always get wrong result ###.half()
+    #     v     =  torch.randn(B,H,S,D2).cuda()#  float16 and bfloat16 always get wrong result ###.half()
+    #     omask =         get_omask(S,H).cuda()#  float16 and bfloat16 always get wrong result ###.half()
+    #     omask = omask[omask!=0]
+    #     records.append([S,H,
+    #                     omask.min().item(), omask.max().item(), omask.half().min().item(), 
+    #                     omask.half().max().item(), omask.bfloat16().min().item(), omask.bfloat16().max().item()])
+
+    # dataframe = pd.DataFrame(records, columns=['S','H','fp32_min','fp32_max','fp16_min','fp16_max','bf16_min','bf16_max'])
+    # print(dataframe)
+    # dataframe.to_csv('gamma_vs_percision.csv',index=False)